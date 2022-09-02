c1920963f419e8d22212d777c2fdeb56f62a650f
Bookja <gfgfgf20@naver.com>
2022-09-02 10:08:31 +0900

    한글 확인

---
+++ src/kosta/mvc/dto/Customer.java
@@ -0,0 +1,17 @@
+package kosta.mvc.dto;
+
+public class Customer {
+
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		 System.out.println("юсх╚а╕"); 
+	}
+
+}
